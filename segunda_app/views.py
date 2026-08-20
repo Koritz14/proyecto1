67199dc4bf84fbab961e1d4b1fcd482c2a1d8daf
unknown <sistemas@VRE_LBM21203.inacap.cl>
2026-08-20 15:39:13 -0400

    avances

--- segunda_app/views.py
+++ segunda_app/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def inicio(request):
-    return HttpResponse("Bienvenido a mi segunda segunda aplicacion")
+    return HttpResponse("<h1>Bienvenido a mi segunda aplicacion</h1>" \
+    "<a href='youtube/' target='_blank'>ver video</a>")
 
 def video_youtube(request):
     return redirect("https://www.youtube.com/watch?v=5qap5aO4i9A")
