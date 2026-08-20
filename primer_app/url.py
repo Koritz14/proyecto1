bab7d5e0dad44756a031d221b4e3db0a188eceb2
unknown <sistemas@VRE_LBM21203.inacap.cl>
2026-08-20 14:35:33 -0400

    views y urls añadidas

---
+++ primer_app/url.py
@@ -0,0 +1,8 @@
+from django import path
+from . import views # el . significa que se importa desde el mismo directorio
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('ahora/', views.mostrar_fecha_hora, name='mostrar_fecha_hora'),
+    path('inacap/', views.acceder_inacap, name='acceder_inacap'),
+]
--- primer_app/views.py
+++ primer_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+def inicio(request):
+    return HttpResponse("<h1>Bienvenido a mi aplicacion<h1>")
+
+#mostrar fecha y hora actual
+def mostrar_fecha_hora(request):
+    ahora = datetime.datetime.now()
+    return HttpResponse(f"Fecha y hora actual: {ahora.strftime('%Y-%m-%d %H:%M:%S')}")
+
+# acceder a portal URL de inacap
+def acceder_inacap(request):
+    return HttpResponse("<a href='https://www.inacap.cl/' target='_blank'>Ir a Inacap</a>")
